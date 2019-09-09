70851a0485494297604dfe6001d82f83d053f3d3
Sarah Rhianna Grosse <sarahrgrosse@sarahs-air.attlocal.net>
2019-09-09 10:46:39 -0400

    updated apis

--- controllers/translate-my-pic-controller.js
+++ controllers/translate-my-pic-controller.js
@@ -3,7 +3,69 @@ var express = require("express");
 var router = express.Router();
 var db = require("../models");
 
+//Routes
+//This route will performs a POST request to create a new user
+router.post("/users", function (req, res) {
+    db.User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(function (dbTranslate) {
+        res.json(dbTranslate);
+    });
+});
 
+//This route will perform a GET request to authenticate the user so they can log in
+router.get("/auth", function (req, res) {
+    db.User.findOne({
+        where:
+        {
+            email: req.body.email,
+            password: req.body.password
+        }
+    }).then(function (dbTranslate) {
+        if (dbTranslate == null) {
+            res.json({
+                "Outcome Code": 401,
+                "Outcome Message": "Incorrect Email or Password."
+            });
+        }
+        else {
+            res.json(
+                {
+                    "Outcome Code": 200,
+                    "Outcome Message": "Authentication Successful!",
+                    "Token": dbTranslate.id
+                }
+            );
+        }
+
+    });
+});
+
+
+//This route performs a GET request to retreive all translations for a user
+router.get("/translations", function (req, res) {
+    db.Translation.findAll({}).then(function (dbTranslate) {
+        res.json(dbTranslate); //Returns a JSON array of all translations
+    });
+});
+
+//This route performs a POST request to create a new translation record
+router.post("/translations", function (req, res) {
+    db.Translation.create({
+        user_id: req.body.user_id,
+        translated_language: req.body.translated_language,
+        analyzed_keywords: req.body.analyzed_keywords,
+        translated_keywords: req.body.translated_keywords
+    }).then(function (dbTranslate) {
+        res.json({
+            "Outcome Code": 200,
+            "Outcome Message": "Translation has been saved."
+        });
+    });
+});
 
 // Exports the translate-my-pic-plus API routes
 module.exports = router;
--- models/user-model.js
+++ models/user-model.js
@@ -1,8 +1,8 @@
 // This is the model for each user in the User table of the Translate database
 // There are four user specified fields: first name, last name, email, and password
-// There three system managed fields: id, createdAt, and updatedAt
+// There are three system managed fields: id, createdAt, and updatedAt
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
         first_name:
         {
@@ -22,7 +22,7 @@ module.exports = function(sequelize, DataTypes) {
                 len: [1]
             }
         },
-        email: 
+        email:
         {
             type: DataTypes.STRING,
             allowNull: false,
@@ -41,13 +41,12 @@ module.exports = function(sequelize, DataTypes) {
             }
         }
     },
-    {
-        defaultScope:
         {
-            attributes: { exclude: ["password"] }
-        }
-    });
-    
+            defaultScope:
+            {
+                attributes: { exclude: ["password"] }
+            }
+        });
+
     return User;
-  };
-  
+};
